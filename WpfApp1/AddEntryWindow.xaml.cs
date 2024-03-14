341b029a7f1ceedd41737aa15445505ba86184aa
Matěj Šiška <22IB32_SISKA@spsjc.cz>
2024-03-14 11:26:12 +0100

    Přidejte soubory projektu.

---
+++ WpfApp1/AddEntryWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace diar
+{
+    public partial class AddEntryWindow : Window
+    {
+        public string Record { get; set; }
+
+        public AddEntryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedDate = datePicker.SelectedDate?.ToString("dd.MM.yyyy");
+            if (string.IsNullOrEmpty(selectedDate))
+            {
+                MessageBox.Show("Vyber Datum!!!","POZOR", MessageBoxButton.OK, MessageBoxImage.Stop);
+                return;
+            }
+
+            Record = $"{selectedDate} - {eventTextBox.Text}";
+            DialogResult = true;
+            MessageBox.Show("Gratuluji, Úspěšný zápis", "Informace", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+    
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace diar
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> Records { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Records = new ObservableCollection<string>();
+            listBox.ItemsSource = Records;
+
+        }
+
+        private void AddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            AddEntryWindow addRecordWindow = new AddEntryWindow();
+            addRecordWindow.Owner = this;
+            if (addRecordWindow.ShowDialog() == true)
+            {
+                Records.Add(addRecordWindow.Record);
+            }
+        }
+
+        private void DeleteRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                Records.Remove(listBox.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Vyber záznam ke smazání.");
+            }
+        }
+    }
+}
